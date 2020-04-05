9ea29e39337f657d5b68837f8293c1371755e4a7
Sean <Sean@DESKTOP-SEAN>
2020-04-05 21:10:29 +0900

    테스트 브랜치에서 DefaultWebTest.java 수정

--- defaultWeb/src/test/java/net/escenter/default_web_project/DefaultWebTest.java
+++ defaultWeb/src/test/java/net/escenter/default_web_project/DefaultWebTest.java
@@ -11,13 +11,14 @@ public class DefaultWebTest {
 	@Before
 	public void setUp() throws Exception {
 	}
-
+	
 	@After
 	public void tearDown() throws Exception {
 	}
 
 	@Test
 	public void test() {
+		System.out.println("test 브랜치에서 작업중");
 		fail("Not yet implemented");
 	}
 
